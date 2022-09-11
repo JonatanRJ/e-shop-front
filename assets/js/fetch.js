55c30b381c98c410f2b3111f3f687228384c5fd6
jonatanrj <jonatan.ramirez5280@alumnos.udg.mx>
2022-09-11 14:18:55 -0500

    Change localhost to API IP

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,27 +1,27 @@
 //Get all products
 const getProducts = async () => {
-    return await fetch(`http://192.168.1.69:3000/api/products`)
+    return await fetch(`https://api-bsale-shop.herokuapp.com/api/products`)
     .then(response => response.json())
     .then(data => data);
 }
 
 //Get all categories
 const getCategories = async () => {
-    return await fetch(`http://192.168.1.69:3000/api/products/category/all`)
+    return await fetch(`https://api-bsale-shop.herokuapp.com/api/products/category/all`)
     .then(response => response.json())
     .then(data => data);
 }
 
 //Get the products by name
 const getProductsByName = async ( product ) => {
-    return await fetch(`http://192.168.1.69:3000/api/products/${product}`)
+    return await fetch(`https://api-bsale-shop.herokuapp.com/api/products/${product}`)
     .then(response => response.json())
     .then(data => data);
 } 
 
 //Get the products by category
 const getProductsByCategory = async ( category ) =>{
-    return await fetch(`http://192.168.1.69:3000/api/products/category/${category}`)
+    return await fetch(`https://api-bsale-shop.herokuapp.com/api/products/category/${category}`)
     .then(response => response.json())
     .then(data => data);
 }
